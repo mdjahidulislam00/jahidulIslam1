5714a01220008ac5f5842b9d6ef3698975bf1465
mdjahiulislam <jahidj2255@gmail.com>
2023-02-08 18:58:16 +0600

    connecting to git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="text-4xl text-pink-400 font-bold">Welcome to my web site</h1>
     </div>
   );
 }
